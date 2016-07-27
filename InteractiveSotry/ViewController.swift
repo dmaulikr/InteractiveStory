abfe65e1478392b8d5845d34f3c304cff5220a10
Andrea Miotto <miotto.andrea@icloud.com>
2016-07-27 14:12:04 +0200

    repositioning textfield, managing errors, dismissing keyboard

--- InteractiveSotry/ViewController.swift
+++ InteractiveSotry/ViewController.swift
@@ -4,25 +4,25 @@
 //
 //  Created by Andrea Miotto on 24/07/16.
 //  Copyright © 2016 Andrea Miotto. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var nameTextField: UITextField!
+class ViewController: UIViewController, UITextFieldDelegate {
     
     enum Error: ErrorProtocol {
         case NoName
     }
     
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var textFieldBottomConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillShow(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillHide(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -30,9 +30,7 @@ class ViewController: UIViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "startAdventure" {
-            
-            
-            do {
+           do {
                 if let name = nameTextField.text {
                     if name == "" {
                         throw Error.NoName
@@ -40,14 +38,54 @@ class ViewController: UIViewController {
                     if let pageController = segue.destinationViewController as? PageController {
                         pageController.page = Adventure.story(name: name)
                     }
+                    
                 }
-            }
-            catch {
+            } catch Error.NoName {
+                let alertController = UIAlertController(title: "Name Not Provided", message: "Provide a name to start your story!", preferredStyle: .alert)
+                let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alertController.addAction(action)
                 
+                present(alertController, animated: true, completion: nil)
+            } catch let error {
+                fatalError("\(error)")
+            }
+            
+        }
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+       if let userInfoDict = notification.userInfo, let keyboardFrameValue = userInfoDict[UIKeyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardFrame = keyboardFrameValue.cgRectValue()
+            
+            UIView.animate(withDuration: 0.8) {
+                self.textFieldBottomConstraint.constant = keyboardFrame.size.height + 10
+                self.view.layoutIfNeeded()
             }
         }
     }
-
-
+    
+    func keyboardWillHide(notification: NSNotification) {
+       UIView.animate(withDuration: 0.8) {
+            self.textFieldBottomConstraint.constant = 40.0
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    // MARK: - UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
+
+
+
+
+
+
+
+
+
+
 
